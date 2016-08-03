40400c80a9f1757f17250ed253d147c391258373
Darrell Tunnell <darrell.tunnell@reach-contact.com>
2016-08-03 17:15:00 +0100

    Renamed test and set main in package.json

--- dist/test/compiler.test.js
+++ dist/test/compiler.test.js
@@ -9,7 +9,7 @@ describe("NetPackTypescriptCompiler", function () {
             // Arrange
             var classAFileContents = fs.readFileSync('testFiles/moduleA/classa.ts', "utf-8");
             var classBFileContents = fs.readFileSync('testFiles/moduleB/classb.ts', "utf-8");
-            var args = '--module Amd -t es5 --outFile test.js --inlineSourceMap --traceResolution --baseUrl testFiles ';
+            var args = '--module Amd -t es5 --outFile test.js --inlineSourceMap --traceResolution ';
             var webRoot = "testFiles";
             var filePathA = webRoot + "/ModuleA/ClassA.ts";
             var filePathB = webRoot + "/ModuleB/ClassB.ts";
